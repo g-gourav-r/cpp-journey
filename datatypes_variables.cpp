9c21cbd4dd0b5fb0b3bf0e944f52387e5be17f0f
Gourav <gouravrchandra@gmail.com>
2022-03-24 09:36:53 +0530

    Data types and Variables

---
+++ datatypes_variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    char initial = 'R';
+    string name ="Gourav";
+    int age = 19;
+    double height = 5.11;
+    bool isMale = true;
+
+    cout << "Hi, I\'m "<< name << "." << initial << " and I\'m " << age << " years old" << endl;
+    cout << isMale << endl; //bool returns either 0 or 1
+
+}
