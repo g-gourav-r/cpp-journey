fc179311e4db0604fcd03f24b4ebbcd07e7f3e49
Gourav <gouravrchandra@gmail.com>
2022-03-25 10:23:30 +0530

    array

---
+++ array.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int arr[20] = {10,20,30,40,50};  //Procedure remains same to initalize different types of array
+    cout<< arr[3] ;
+    return 0;
+}
