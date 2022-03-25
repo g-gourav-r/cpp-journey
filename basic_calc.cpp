bc2741db74c3e8ca4a222d9c62b57576ba28360b
Gourav <gouravrchandra@gmail.com>
2022-03-25 09:37:16 +0530

    basic calculator with no much feature

---
+++ basic_calc.cpp
@@ -0,0 +1,20 @@
+//bare basic calculator to add two numbers
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    double num1, num2;
+
+    cout << "Enter the first number: ";
+    cin>> num1;
+
+    cout << "Enter the second number: ";
+    cin>> num2;
+
+    cout<< "The sum is "<< num1+num2;
+
+    return 0;
+}
