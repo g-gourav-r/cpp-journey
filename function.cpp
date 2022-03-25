761de23097f6f62587cc09f0d66c5231b100cae7
Gourav <gouravrchandra@gmail.com>
2022-03-25 18:43:51 +0530

    simple function

--- function.cpp
+++ function.cpp
@@ -2,15 +2,15 @@
 
 using namespace std;
 
-void sayHi(string name, int age);
+void sayHi(string name, int age); //function initialization
 
 int main(){
 
-    sayHi("Gourav",21);
+    sayHi("Gourav",21); //function call
 
     return 0;
 }
-
+//function declaration
 void sayHi(string name, int age){
     cout<<"Hello "<<name<<" you are "<<age<<" years old.";
 }
